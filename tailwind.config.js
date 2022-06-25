b0eb131ac3a43112cf96653fc111e98db0dc7c4c
PrateekR16 <prateekranka1607@gmail.com>
2022-06-25 23:30:53 +0530

    initial ui completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage : {
+        "password-bg" : "url('./background.png')"
+      }
+    },
+  },
+  plugins: [],
+}
